103fb0c9f0dbc24c373f4b63996bd17a26685310
Martin Liriano <sprite599@gmail.com>
2024-12-01 23:35:17 -0500

    added message log functionality and a few other features

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 import discord
 from discord.ext import commands
 
-from core import process_message
+from core import process_message, get_message_log, get_user
 
 socket.gethostbyname("")
 TOKEN = os.getenv('FRIEND_TOKEN_DISCORD')
@@ -23,10 +23,20 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    response = process_message(message.content)
+    if message.content == '!crime':
+        response = get_message_log(message.author)
+        await message.channel.send(f'{response}')
+        return
+            
+    user_exist = get_user(message.author)
+
+    response = process_message(message.content, message.author)
     if not response:
         return
-    await message.channel.send(f'{message.author.mention}{response}')
+    await message.channel.send(f'{message.author.mention} {response}')
+
+    if user_exist:
+        await message.delete()
 
 # Run the bot with the token
 bot.run(TOKEN)
--- core.py
+++ core.py
@@ -1,14 +1,56 @@
-bad_words = [
-    'faggot',
-    'bitch',
-    'pussy'
-]
+from datetime import datetime, timedelta
 
-def process_message(message):
+def read_file_to_list(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    # Strip newline characters from each line
+    lines = [line.strip() for line in lines]
+    return lines
+
+bad_words = read_file_to_list('bad_words.txt')
+
+cd_users = {}
+message_log = {}
+
+def process_message(message, user):
     if not message:
         return
-    message = message.lower()
+    message_lower = message.lower()
+    if user in message_log:
+        message_log[user].append(message)
+    else:
+        message_log[user] = []
+        message_log[user].append(message)
     for bad_word in bad_words:
-        if bad_word in message:
-            return f"You've said the {bad_word[0]}-word. You are on CD."
+        if bad_word in message_lower:
+            user_exist = check_user(user)
+            readable_date = datetime.strptime(str(cd_users[user]), '%Y-%m-%d %H:%M:%S.%f')
+            readable_date = readable_date.strftime('%B %d, %Y %I:%M%p')
+            if not user_exist:
+                return f"You've said the {bad_word[0]}-word. You are on CD. Your CD will end {readable_date}"
+            else:
+                return f"**Repeat Offense** You've said the {bad_word[0]}-word. Your CD will be extended. Your CD will end {readable_date}"
     
+def check_user(user):
+    if user not in cd_users:
+        cd_users[user] = datetime.now() + timedelta(hours=1)
+        return None
+    cd_users[user] = cd_users[user] + timedelta(hours=1)
+    return True
+
+def remove_cd(user):
+    if user not in cd_users:
+        return
+    if datime.now() > cd_users[user]:
+        del cd_users[user]
+
+def get_user(user):
+    return user in cd_users
+
+def get_message_log(user):
+    if user not in message_log:
+        return 'You are not on CD.'
+    response = ''
+    for messages in message_log[user]:
+        response += messages + '\n'
+    return response
